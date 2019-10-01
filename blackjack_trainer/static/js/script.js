ca29bb3a8c254caa47a12efc08e3fa3c78a5c4a0
cberry216 <christopherberry216@gmail.com>
2019-10-01 08:56:27 -0400

    Finalized BJBS modal functionality and design.

--- blackjack_trainer/static/js/script.js
+++ blackjack_trainer/static/js/script.js
@@ -1,15 +1,31 @@
 function bjbs_modal() {
   let bjbs_button = document.querySelector('.welcome__strategy');
+  let bjbs_close = document.querySelector('.bjbs__close');
   let dim_screen = document.querySelector('.dim');
   let bjbs_modal = document.querySelector('.bjbs');
+
   bjbs_button.addEventListener('click', () => {
     dim_screen.classList.toggle('dim--open');
     bjbs_modal.classList.toggle('bjbs--open');
   })
+  bjbs_close.addEventListener('click', (e) => {
+    // Stop Event Bubbling (inner on-click event firing outter on-click event).
+    if (!e) var e = window.event;
+    e.cancelBubble = true;
+    if (e.stopPropagation) e.stopPropagation();
+    dim_screen.classList.toggle('dim--open');
+    bjbs_modal.classList.toggle('bjbs--open');
+  })
+  bjbs_modal.addEventListener('click', () => {
+    dim_screen.classList.toggle('dim--open');
+    bjbs_modal.classList.toggle('bjbs--open');
+  })
+
+
 }
 
 function run() {
-
+  bjbs_modal();
 }
 
 window.onload = run
